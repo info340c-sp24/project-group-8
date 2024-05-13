9e0a36bd291180deb4b6ddbf47a641a587819f87
Allen Yuan <129093001+AllenR6S@users.noreply.github.com>
2024-05-13 12:52:59 -0700

    add workout log in App
    added workout log in app

--- src/javascript/App.jsx
+++ src/javascript/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Recommendations from './recs';
 import Index from './index';
+import Workout_log from './workout-log';
 
 function App() {
     return (
       <div>
         <Recommendations />
+        <Workout_log />
         <Index />
         {/* import other pages here*/}
       </div>
     );
   }
-  
+
   export default App;

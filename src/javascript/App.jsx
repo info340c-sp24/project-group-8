8401b943420d0cfce1306cceda90b41fb5643f79
sbains2 <97136840+sbains2@users.noreply.github.com>
2024-05-15 17:49:52 -0700

    firebase-deployed

--- src/javascript/App.jsx
+++ src/javascript/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Recommendations from './recs';
-import Index from './index';
+import Index from '../index';
 import Workout_log from './workout-log';
 
 function App() {

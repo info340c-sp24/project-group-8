624a9e617c92eb63e3a867cc59facd4efa3fe78e
Skykasih <skykasih@uw.edu>
2024-05-20 19:09:48 -0700

    INDEX JS FIX

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import * as Helper from './javascript/helper.js';
 
-const root = ReactDOM.createRoot(Helper.id('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>

ec5739326177518b9e0e99e68946f891493b0492
Skykasih <skykasih@uw.edu>
2024-05-21 19:30:09 -0700

    Firebase Solved

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ import './css/template.css';
 import { BrowserRouter } from 'react-router-dom'
 // import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJX9a4iBbqPx3jUt5q387TRb0rO3kuF_M",
+  authDomain: "m-f-u-i.firebaseapp.com",
+  projectId: "m-f-u-i",
+  storageBucket: "m-f-u-i.appspot.com",
+  messagingSenderId: "1067112540809",
+  appId: "1:1067112540809:web:d47620d088e43db1723458"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
